5d04f0603cc288c194fb43b16655611429e6a036
cpantoga <charles.pantoga@rumble.com>
2023-05-03 20:25:41 -0500

    Automatically add SSH Agent forwarding to ssh-config

--- install.sh
+++ install.sh
@@ -2,3 +2,20 @@
 # (c) 2023 github.com/varcharlie
 cat ssh-agent-keepalive >> ~/.bash_profile && echo "cat ssh-agent-keepalive >> ~/.bash_profile"
 
+if ! test -f ~/.ssh/config; then
+    {
+        >>~/.ssh/config cat<<-EOF
+		Host *
+		    ForwardAgent yes
+		EOF
+    } && echo "Created ~/.ssh/config with AgentForwarding enabled"
+else
+    echo
+    echo
+    echo "Found config at ~/.ssh/config:"
+    echo "Please ensure to add 'ForwardAgent yes' to all hosts"
+    echo
+    echo "Example ~/.ssh/config:"
+    echo "Host *"
+    echo "    ForwardAgent yes"
+fi
